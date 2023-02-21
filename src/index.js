b2b98c758b48b1d906fe874ae7953e1e2028d837
MariaIsaDurango <107730198+MariaIsaDurango@users.noreply.github.com>
2023-02-21 14:50:48 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-import validator from './validator.js';
+import validator from "./validator.js";
+//console.log(validator);
 
-console.log(validator);
+// valor input
+const crediCard = document.getElementById("crediCard");
+const btncrediCard = document.getElementById("click");
+btncrediCard.addEventListener("click", validate);
+
+function validate() {
+  // console.log("provando", isNaN(crediCard.value));
+  //identifica valor ingresado en input sea caracter numerico
+  if (isNaN(crediCard.value)) {
+    document.getElementById("validate").innerHTML =
+      "ingresar solo datos numericos";
+    return;
+  } else {
+    validator.isValid(crediCard);
+  }
+}
